ebfda678f08d7f764e28cc261f0b6070cb85b5df
polavskiran <36887402+polavskiran@users.noreply.github.com>
2021-09-21 17:38:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GetCourses from "./components/GetCourses";
+
+function App() {
+  return (
+    <div className="App">
+        <GetCourses />
+    </div>
+  );
+}
+
+export default App;
